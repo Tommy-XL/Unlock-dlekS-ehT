4d193f49b4fd97f72166c061ee0d56035e8db5a3
Tommy XL <fadeevvladimir16@gmail.com>
2024-01-08 04:20:59 +0800

    Add Mod Icon

---
+++ Patches/ModIconPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace UnlockDleks.Patches;
+
+[HarmonyPatch(typeof(ModManager), nameof(ModManager.LateUpdate))]
+class ModManagerLateUpdatePatch
+{
+    public static void Prefix(ModManager __instance)
+    {
+        __instance.ShowModStamp();
+    }
+    public static void Postfix(ModManager __instance)
+    {
+        var offset_y = HudManager.InstanceExists ? 1.6f : 1.4f;
+        __instance.ModStamp.transform.position = AspectPosition.ComputeWorldPosition(
+            __instance.localCamera, AspectPosition.EdgeAlignments.RightTop,
+            new Vector3(0.4f, offset_y, __instance.localCamera.nearClipPlane + 0.1f));
+    }
+}
--- Patches/VentArrowsPatch.cs
+++ Patches/VentArrowsPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 using UnlockDleks.Modules;
 
 namespace UnlockDleks.Patches;
